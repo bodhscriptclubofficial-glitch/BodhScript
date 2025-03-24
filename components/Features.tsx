11a52ae39977718ef2709a8fdbcb28e9e1b700b3
Divya <singhdivya10aug2002@gmail.com>
2025-03-25 00:31:24 +0530

    codequest

--- components/Features.tsx
+++ components/Features.tsx
@@ -9,7 +9,7 @@ interface Feature {
 
 const features: Feature[] = [
   {
-    image: '/images/Poster.png',
+    image: '/images/codeq2.jpg',
   },
 ]
 
@@ -50,22 +50,16 @@ export default function Features() {
           <span className='text-cyber-pink pl-4'>Events</span>
         </h2>
 
-        {/* CTA Button */}
-        {/* <Link href='/events'>
-          <span className='bg-cyber-pink text-white px-6 py-3 rounded-full font-semibold hover:bg-cyber-blue hover:text-cyber-black transition duration-300 animate-strong-pulse mt-8 inline-block cursor-pointer'>
-            Check Events
-          </span>
-        </Link> */}
-
         {/* Feature Cards - Centered Grid */}
-        <div className='flex justify-center items-center w-full mt-12'>
+
+        <div className='flex justify-center items-center w-full mt-12 pt-10'>
           <div className='grid grid-cols-1 gap-8 max-w-[700px] mx-auto'>
             {features.map((feature, index) => (
               <div
                 key={index}
                 className='bg-cyber-black p-8 rounded-lg border border-gray-600 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-cyber-blue/50 flex justify-center items-center h-[550px] w-[700px] mx-auto'
               >
-                {/* Image Container */}
+                {/* Image container */}
                 <div className='relative w-full h-full flex items-center justify-center'>
                   <img
                     src={feature.image}
@@ -84,3 +78,25 @@ export default function Features() {
     </section>
   )
 }
+
+/*
+       <div className='flex justify-center items-center w-full mt-12'>
+          <div className='grid grid-cols-1 gap-8 max-w-[700px] mx-auto'>
+            {features.map((feature, index) => (
+              <div
+                key={index}
+                className='bg-cyber-black p-8 rounded-lg border border-gray-600 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-cyber-blue/50 flex justify-center items-center h-[550px] w-[700px] mx-auto'
+              >
+                {Image Container }
+                <div className='relative w-full h-full flex items-center justify-center'>
+                  <img
+                    src={feature.image}
+                    alt='Event'
+                    className='w-full h-full object-contain rounded-md hover:cursor-pointer'
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+*/
